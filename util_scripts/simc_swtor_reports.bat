c8ac263ece911fcfe52a5f0f2273553689157416
Casey Carter <cartec69@gmail.com>
2012-03-13 10:43:07 -0500

    Cleanup simc_swtor_reports.bat a little.

--- util_scripts/simc_swtor_reports.bat
+++ util_scripts/simc_swtor_reports.bat
@@ -1,25 +1,22 @@
-set simc_path=..
+@echo off
+setlocal
+set PATH=..\engine;engine;%PATH%
 set iterations=100000
 set calculate_scale_factors=1
 set threads=4
 set center_scale_delta=1
 
-set simulation1=Sage_Sorcerer_Raid
-set simulation1_options=
+call :runsim Sage_Sorcerer_Raid
+call :runsim Sage_Sorcerer_Raid -HelterSkelter "fight_style=HelterSkelter"
+call :runsim Sage_Sorcerer_Raid -HeavyMovement "fight_style=HeavyMovement"
+call :runsim Assassin_Raid
+REM call :runsim Assassin_Raid -HelterSkelter "fight_style=HelterSkelter"
+REM call :runsim Assassin_Raid -HeavyMovement "fight_style=HeavyMovement"
 
-call %simc_path%\engine\simc.exe %simulation1%.simc %simulation1_options% iterations=%iterations% calculate_scale_factors=%calculate_scale_factors% center_scale_delta=%center_scale_delta% threads=%threads% html=%simc_path%\%simulation1%%simulation1_options%.html
+goto :eof
 
-set simulation1=Sage_Sorcerer_Raid
-set simulation1_options="fight_style=HelterSkelter"
-
-call %simc_path%\engine\simc.exe %simulation1%.simc %simulation1_options% iterations=%iterations% calculate_scale_factors=%calculate_scale_factors% center_scale_delta=%center_scale_delta% threads=%threads% html=%simc_path%\%simulation1%%simulation1_options%.html
-
-set simulation1=Sage_Sorcerer_Raid
-set simulation1_options="fight_style=HeavyMovement"
-
-call %simc_path%\engine\simc.exe %simulation1%.simc %simulation1_options% iterations=%iterations% calculate_scale_factors=%calculate_scale_factors% center_scale_delta=%center_scale_delta% threads=%threads% html=%simc_path%\%simulation1%%simulation1_options%.html
-
-set simulation1=Assassin_Raid
-set simulation1_options=
-
-call %simc_path%\engine\simc.exe %simulation1%.simc %simulation1_options% iterations=%iterations% calculate_scale_factors=%calculate_scale_factors% center_scale_delta=%center_scale_delta% threads=%threads% html=%simc_path%\%simulation1%%simulation1_options%.html
+:runsim
+echo.%1%2
+call simc.exe %1.simc output=NUL: %3 iterations=%iterations% calculate_scale_factors=%calculate_scale_factors% center_scale_delta=%center_scale_delta% threads=%threads% html=%1%2.html
+echo.
+goto :eof
