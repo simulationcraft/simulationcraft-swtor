7151d2c8e5734a908472d7dd437a5eef4b0ed3c4
harly <wowharly@gmail.com>
2012-05-28 17:53:33 +1000

    Agent: whitespace

--- engine/class_modules/agent_smug.cpp
+++ engine/class_modules/agent_smug.cpp
@@ -89,6 +89,28 @@ struct explosive_probe_t : public tech_attack_t
   }
 };
 
+// Orbital Strike | XS Freighter Flyby ======================================
+struct orbital_strike_t : public tech_attack_t
+{
+  orbital_strike_t( class_t* p, const std::string& n, const std::string& options_str) :
+      tech_attack_t( n, p, SCHOOL_ELEMENTAL )
+  {
+    parse_options( options_str );
+
+    base_cost                   = 30;
+    range                       = 30.0;
+    cooldown -> duration        = from_seconds( 60 );
+    td.standardhealthpercentmin =
+    td.standardhealthpercentmax = 0.177;
+    td.power_mod                = 1.77;
+    num_ticks                   = 3; // TODO: sniper set bonus? +1 tick
+    base_tick_time              = from_seconds( 3 );
+    base_execute_time           = from_seconds( 3 );
+
+    aoe = 99; // TODO FIX: unlimited. "all targets in area"
+  }
+};
+
 
 // Rifle Shot | Flurry Of Bolts =========================================================
 struct rifle_shot_t : public range_attack_t
@@ -147,27 +169,6 @@ struct rifle_shot_t : public range_attack_t
   }
 };
 
-// Orbital Strike | XS Freighter Flyby ======================================
-struct orbital_strike_t : public tech_attack_t
-{
-  orbital_strike_t( class_t* p, const std::string& n, const std::string& options_str) :
-      tech_attack_t( n, p, SCHOOL_ELEMENTAL )
-  {
-    parse_options( options_str );
-
-    base_cost                   = 30;
-    range                       = 30.0;
-    cooldown -> duration        = from_seconds( 60 );
-    td.standardhealthpercentmin =
-    td.standardhealthpercentmax = 0.177;
-    td.power_mod                = 1.77;
-    num_ticks                   = 3; // TODO: sniper set bonus? +1 tick
-    base_tick_time              = from_seconds( 3 );
-    base_execute_time           = from_seconds( 3 );
-
-    aoe = 99; // TODO FIX: unlimited. "all targets in area"
-  }
-};
 
 
 // Snipe | Charged Burst ====================================================
