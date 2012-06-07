d55865a671db427d679f870d9249d3c4c26c621d
harly <wowharly@gmail.com>
2012-06-07 17:01:15 +1000

    holding a local reference to the debuff inside sniper class works so looks like targetdata() is returning something odd?

--- engine/class_modules/sc_gunslinger_sniper.cpp
+++ engine/class_modules/sc_gunslinger_sniper.cpp
@@ -15,6 +15,7 @@ namespace { // ANONYMOUS ====================================================
 namespace gunslinger_sniper { // ============================================
 
 class class_t;
+buff_t* _test;
 
 class targetdata_t : public agent_smug::targetdata_t
 {
@@ -231,6 +232,7 @@ targetdata_t::targetdata_t( class_t& source, player_t& target ) :
   add( *debuff_cluster_bombs       );
   add( *debuff_electrified_railgun );
   add( *debuff_shatter_shot        );
+  _test = debuff_shatter_shot;
   debuff_shatter_shot -> __test = 6;
 
   add( dot_electrified_railgun     );
@@ -1015,10 +1017,12 @@ struct shatter_shot_t : public range_attack_t
   virtual void execute()
   {
     base_t::execute();
-    targetdata() -> debuff_shatter_shot -> trigger();
-    std::cout << "XXXshatter_shot_t execute test value: " << targetdata() -> debuff_shatter_shot -> __test << std::endl;
-    targetdata() -> debuff_shatter_shot -> __test = 4;
-    std::cout << "XXXshatter_shot_t execute test value: " << targetdata() -> debuff_shatter_shot -> __test << std::endl;
+    //buff_t* shatter = targetdata() -> debuff_shatter_shot;
+    buff_t* shatter = _test;
+    shatter -> trigger();
+    shatter -> __test = 4;
+    std::cout << "XXXshatter_shot_t execute direct test value: " << shatter -> __test << std::endl;
+    std::cout << "XXXshatter_shot_t execute targetdata test value: " << targetdata() -> debuff_shatter_shot -> __test << std::endl;
   }
 };
 
@@ -1339,7 +1343,7 @@ void class_t::init_actions()
 
     action_list_str += sl + abilities.take_cover + ",if=buff." + abilities.cover + ".down";
     // strictly we would only do this if there weren't already 5 armor debuffs on...
-    action_list_str += sl + abilities.shatter_shot + ",if=buff.shatter_shot.up";
+    action_list_str += sl + abilities.shatter_shot + ",if=!buff.shatter_shot.stack";
     action_list_str += "/use_relics/power_potion";
     // guessing priority and optimal energy
     if ( talents.plasma_probe -> rank() )
