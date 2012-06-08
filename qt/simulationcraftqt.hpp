2d87e2fcb0dd3dbb309c45fe2d080b53e8998d23
harly <wowharly@gmail.com>
2012-06-08 22:59:00 +1000

    GUI: support armor debuffs each stacking 0-5, defaulting to 3x1x1 as the sim does

--- qt/simulationcraftqt.hpp
+++ qt/simulationcraftqt.hpp
@@ -137,6 +137,9 @@ public:
   QListWidget* itemDbOrder;
   QButtonGroup* buffsButtonGroup;
   QButtonGroup* debuffsButtonGroup;
+  QComboBox* shatterChoice;
+  QComboBox* sunderChoice;
+  QComboBox* heatsigChoice;
   QButtonGroup* scalingButtonGroup;
   QComboBox* plotsPointsChoice;
   QComboBox* plotsStepChoice;
