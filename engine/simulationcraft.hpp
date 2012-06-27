774209318fec78963459bd13a1c0b1c95da3e3bc
harly <wowharly@gmail.com>
2012-06-27 22:46:22 +1000

    Version bump to 130-2

--- engine/simulationcraft.hpp
+++ engine/simulationcraft.hpp
@@ -81,7 +81,7 @@
 #define PRINTF_ATTRIBUTE(a,b) __attribute__((format(printf,a,b)))
 
 #define SC_MAJOR_VERSION "130"
-#define SC_MINOR_VERSION "1"
+#define SC_MINOR_VERSION "2"
 #define SWTOR_VERSION_LIVE "1.3.0"
 #define SWTOR_VERSION_PTR "1.3.0"
 #define SC_USE_PTR ( 0 )
