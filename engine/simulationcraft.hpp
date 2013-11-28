8bcb8e82ec7b1ee2b6235cc085b4481ff2d229f1
philoptik <philoptik@gmail.com>
2013-11-28 20:59:38 +0100

    Update Version to 2.4.3

--- engine/simulationcraft.hpp
+++ engine/simulationcraft.hpp
@@ -80,10 +80,10 @@
 
 #define PRINTF_ATTRIBUTE(a,b) __attribute__((format(printf,a,b)))
 
-#define SC_MAJOR_VERSION "201"
+#define SC_MAJOR_VERSION "243"
 #define SC_MINOR_VERSION "1"
-#define SWTOR_VERSION_LIVE "2.0.1"
-#define SWTOR_VERSION_PTR "2.1.0"
+#define SWTOR_VERSION_LIVE "2.4.3"
+#define SWTOR_VERSION_PTR "2.4.3"
 #define SC_USE_PTR ( 0 )
 #define SC_BETA ( 0 )
 #define SC_EPSILON ( 0.000001 )
